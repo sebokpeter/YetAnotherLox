1b4804ebbc50da5bd04fa553e5cb04a039262a85
Péter Sebők <sebok.peter97@gmail.com>
2024-01-28 13:54:36 +0100

    Renaming.

--- Frontend.Test/Scanner/ScannerEmptySourceTests.cs
+++ Frontend.Test/Scanner/ScannerEmptySourceTests.cs
@@ -3,11 +3,7 @@ using Shared;
 
 namespace Frontend.Test.Scanner;
 
-/// <summary>
-/// Unit tests for the <see cref="Scanner">.
-/// Test if the scanner can convert a string (the source) into a stream of <see cref="Token"/>s properly.
-/// </summary>
-public class ScannerTests
+public class ScannerEmptySourceTests
 {
     [Fact]
     public void EmptyString_Returns_IEnumerable_Containing_EoFToken()
@@ -32,12 +28,4 @@ public class ScannerTests
 
         tokens.Should().ContainSingle(t => t.Line == line && t.Type == TokenType.EOF);
     }
-
-    // TODO: expand testing to ensure that the scanner properly produces tokens
-    // - Error handling 
-    // - Test for all constructs
-    //      * Variable declarations
-    //      * Class declarations
-    //      * Function declarations
-    //      * Etc.
 }
